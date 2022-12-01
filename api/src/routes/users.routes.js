247d0dc9af7dcfc7a703d50ef2b2763d647de0df
Ícaro Felizardo <60118701+Icaro-Felizardo@users.noreply.github.com>
2022-11-30 22:21:43 -0300

    cadastro de usuários e alteração de informações no bd

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -4,16 +4,15 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
-
 function myMiddleware(request, response, next){
     console.log("passou no middleware")
 
     next()
 }
 
-
 const usersController = new UsersController()
 
 usersRoutes.post("/",myMiddleware, usersController.create)
+usersRoutes.put("/:id", usersController.update)
 
 module.exports = usersRoutes;
