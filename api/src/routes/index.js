f90f1faf28e3d5f0fd852a158b7d7c02e747c647
Ícaro Felizardo <60118701+Icaro-Felizardo@users.noreply.github.com>
2023-01-05 22:46:11 -0300

    Rota de autenticação

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,15 @@
 const { Router } = require("express")
 
-const usersRoutes = require("./users.routes.js")
-const notesRoutes = require("./notes.routes.js")
-const tagsRoutes = require("./tags.routes.js")
+const usersRoutes = require("./users.routes.js");
+const notesRoutes = require("./notes.routes.js");
+const tagsRoutes = require("./tags.routes.js");
+const sessionsRoutes = require("./sessions.routes.js");
 
-const routes = Router()
+const routes = Router();
 
-routes.use("/users", usersRoutes)
-routes.use("/notes", notesRoutes)
-routes.use("/tags", tagsRoutes)
+routes.use("/users", usersRoutes);
+routes.use("/notes", notesRoutes);
+routes.use("/tags", tagsRoutes);
+routes.use("/sessions", sessionsRoutes);
 
-module.exports = routes
+module.exports = routes;
